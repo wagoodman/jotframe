3c19738e314aff7ab156545257d59feb4ed09069
Phil <phil.boothe@gmail.com>
2018-10-04 19:01:55 -0400

    Add functionality to TopFrame

--- top_frame.go
+++ top_frame.go
@@ -1,13 +1,14 @@
 package jotframe
 
-
 func NewTopFrame(rows int, hasHeader, hasFooter bool) *TopFrame {
-	innerFrame := newLogicalFrameAt(rows, hasHeader, hasFooter, 1)
+	logicalFrame := newLogicalFrameAt(rows, hasHeader, hasFooter, 0)
 	frame := &TopFrame{
-		frame: innerFrame,
-		lock: getScreenLock(),
+		frame: logicalFrame,
+		lock:  getScreenLock(),
 	}
-	frame.frame.updateFn = frame.update
+
+	// TODO: Add this later
+	frame.frame.updateFn = nil
 
 	frame.lock.Lock()
 	defer frame.lock.Unlock()
@@ -31,32 +32,32 @@ func (frame *TopFrame) Lines() []*Line {
 func (frame *TopFrame) Append() (*Line, error) {
 	frame.lock.Lock()
 	defer frame.lock.Unlock()
-
 	defer frame.frame.updateAndDraw()
+
 	return frame.frame.append()
 }
 
 func (frame *TopFrame) Prepend() (*Line, error) {
 	frame.lock.Lock()
 	defer frame.lock.Unlock()
-
 	defer frame.frame.updateAndDraw()
+
 	return frame.frame.prepend()
 }
 
 func (frame *TopFrame) Insert(index int) (*Line, error) {
 	frame.lock.Lock()
 	defer frame.lock.Unlock()
-
 	defer frame.frame.updateAndDraw()
+
 	return frame.frame.insert(index)
 }
 
 func (frame *TopFrame) Remove(line *Line) error {
 	frame.lock.Lock()
 	defer frame.lock.Unlock()
-
 	defer frame.frame.updateAndDraw()
+
 	return frame.frame.remove(line)
 }
 
@@ -72,7 +73,6 @@ func (frame *TopFrame) Close() error {
 func (frame *TopFrame) Clear() error {
 	frame.lock.Lock()
 	defer frame.lock.Unlock()
-
 	defer frame.frame.updateAndDraw()
 
 	return frame.frame.clear()
@@ -81,25 +81,25 @@ func (frame *TopFrame) Clear() error {
 func (frame *TopFrame) ClearAndClose() error {
 	frame.lock.Lock()
 	defer frame.lock.Unlock()
-
 	defer frame.frame.updateAndDraw()
 
 	err := frame.frame.clear()
 	if err != nil {
 		return err
 	}
-	return frame.frame.close()
-}
 
-// update any positions based on external data and redraw
-func (frame *TopFrame) update() error {
-	if frame.frame.frameStartIdx != 1 {
-		offset := 1 - frame.frame.frameStartIdx
-		return frame.frame.move(offset)
-	}
-	return nil
+	return frame.frame.close()
 }
 
 func (frame *TopFrame) Wait() {
 	frame.frame.wait()
 }
+
+// // update any positions based on external data and redraw
+// func (frame *TopFrame) update() error {
+// 	if frame.frame.frameStartIdx != 1 {
+// 		offset := 1 - frame.frame.frameStartIdx
+// 		return frame.frame.move(offset)
+// 	}
+// 	return nil
+// }
